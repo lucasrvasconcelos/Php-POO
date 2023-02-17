ca6a44863af2eacff595b90a06730293f8796d2b
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-02-17 16:15:08 -0300

    Poo

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var imgs = document.querySelectorAll('img')
+var body = document.querySelector('body')
+var btnClose = document.createElement('button')
+
+var btnDiv = document.querySelector('.btn')
+
+btnClose.innerHTML = 'Fechar'
+imgs.forEach((e) => {
+    e.addEventListener('click', () => {
+        e.classList.toggle("view")
+    })
+})
+
+body.addEventListener('click', () => {
+    imgs.forEach((e) => {
+        if(e.getAttribute('class') == 'view'){
+            body.addEventListener('click', () =>{
+                e.classList.toggle("view")
+            })
+        }
+    })
+})
+
+
+
+
